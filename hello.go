3febdc0c10da36553319b2dd4fb5569aecba4515
Brenda David <brenda.silveira@dbserver.com>
2022-11-17 17:29:23 -0300

    first commit

---
+++ hello.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nome string = "Douglas"
+	var idade int = 24
+	var versao float32 = 1.1
+	fmt.Println("Olá sr.", nome, "sua idade é:", idade)
+	fmt.Println("O projeto está na versão", versao)
+}
