9c1b4583d7202de0499aa5dff9d55bd3500ca150
Brenda David <brenda.silveira@dbserver.com>
2022-11-17 17:55:41 -0300

    adiciona menu e comando scanner de variável

--- hello.go
+++ hello.go
@@ -1,19 +1,21 @@
 package main
 
-import (
-	"fmt"
-	"reflect"
-)
+import "fmt"
 
 func main() {
 	// O Go entende que a variável é do tipo string pois recebe uma String e a palavra chave var pode ser removida e substituida por :=
 	nome := "Douglas"
 	// No caso do float, como existem dois tipos, o Go vai inferir que o tipo da variável será float64
 	versao := 1.1
+
 	fmt.Println("Olá sr.", nome)
 	fmt.Println("O projeto está na versão", versao)
 
-	// O pacote Reflect tem uma função para saber os tipos das variáveis
-	fmt.Println("Variável nome é do tipo:", reflect.TypeOf(nome), "Variável versao é to tipo:", reflect.TypeOf(versao))
-	// Saída: Variável nome é do tipo: string Variável versao é to tipo: float64
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+
+	// O comando Scanf precisa receber o modificador com o tipo de variável e o endereço(ponteiro) da variável
+	var comando int
+	fmt.Scanf("%d", &comando)
 }
